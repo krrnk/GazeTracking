96b0f4bb7be36b96ef43e74082223b63868532b5
camilo <info@krrnk.com>
2022-04-10 22:45:47 +0300

    using middle point between eyes

--- eye_tracking.py
+++ eye_tracking.py
@@ -8,6 +8,18 @@ import numpy as np
 from gaze_tracking import GazeTracking
 from pythonosc.udp_client import SimpleUDPClient
 
+def middle_point(coords_A, coords_B):
+    if type(coords_A) and type(coords_B) is tuple:
+        x_coord = (coords_A[0] + coords_B[0]) * 0.5
+        y_coord = (coords_A[1] + coords_B[1]) * 0.5
+        return int(x_coord), int(y_coord)
+    else:
+        return None 
+
+def draw_middle_point(middle_point, frame):
+    cv2.circle(frame, middle_point, radius=4, color=(0, 0, 255), thickness=-1)
+    cv2.putText(frame, "Middle point: " + str(middle_point), (90, 195), 
+                cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
 
 def gaze_track(gaze, webcam):
     # We get a new frame from the webcam
@@ -28,16 +40,23 @@ def gaze_track(gaze, webcam):
     elif gaze.is_center():
         text = "Looking center"
 
-    cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+    cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 
+                1.6, (147, 58, 31), 2)
 
     left_pupil = gaze.pupil_left_coords()
     right_pupil = gaze.pupil_right_coords()
-    cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-    cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), 
+                cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), 
+                cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+
+    gaze_focus = middle_point(left_pupil, right_pupil)
+    draw_middle_point(gaze_focus, frame)
 
     cv2.imshow("Demo", frame)
 
-    return left_pupil, right_pupil
+    return gaze_focus
+
 
 
 if __name__ == '__main__':
@@ -49,12 +68,10 @@ if __name__ == '__main__':
     client = SimpleUDPClient(ip, port)
 
     while True:
-        left_pupil, right_pupil = gaze_track(gaze, webcam)
-        
-        if type(left_pupil) and type(right_pupil) is tuple:
-            left_coords = [item.tolist() for item in left_pupil]
-            right_coords = [item.tolist() for item in right_pupil]
-            client.send_message('/gaze_track', [left_coords, right_coords])
+        gaze_centre = gaze_track(gaze, webcam)
+       
+        if type(gaze_centre) is tuple:
+            client.send_message('/gaze_track', [gaze_centre[0], gaze_centre[1]])
 
 
         if cv2.waitKey(1) == 27:
